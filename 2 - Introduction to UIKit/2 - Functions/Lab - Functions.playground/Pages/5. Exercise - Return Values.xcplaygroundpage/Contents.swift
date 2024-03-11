00ddd9b4a6acfa99e2c67a5f21ef5b36c9bf96d0
Vanshaj Saxena <vs110405@outlook.com>
2024-03-11 11:54:37 +0530

    Module 2 Introduction to UIKit: Completed till 2 Functions
    Stirngs and Functions are completed by now

--- 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,21 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Sophie" the return value might be "Hi, Sophie! How are you?" Use the function and print the result.
  */
+func greeting(name: String) -> String {
+    var greet = "Hi, \(name)! How are you?"
+   print(greet)
+    return greet
+}
+greeting(name:"Vanshaj")
 
 
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
-
-
+func myMathFunc(arg1: Int, arg2: Int) -> Int {
+    var result = (arg1 * arg2) + 2
+    print(result)
+    return result
+}
+myMathFunc(arg1: 20, arg2:30 )
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
  */
--- 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
@@ -7,11 +7,25 @@
 
  As an example, write a function that only does a portion of what your previous `pacing` function did. This function will be called `calculatePace`. It should take three `Double` arguments called `currentDistance`, `totalDistance`, and `currentTime`, and should return a `Double` that will represent the time at which the user will finish the run based on the user's current distance and time. call the function and print the return value.
  */
-
-
+func calculatePace(currentDistance: Double, currentTime: Double, totalDistance: Double ) -> Double{
+    var time = totalDistance/(currentDistance/currentTime)
+    print(time)
+    return time
+}
+calculatePace(currentDistance: 5000, currentTime: 30, totalDistance: 10000)
 //:  Now write a function called `pacing` that takes four `Double` arguments called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. The function should also return a `String`, which will be the message to show the user. The function should call `calculatePace`, passing in the appropriate values, and capture the return value. The function should then compare the returned value to `goalTime` and if the user is on pace return "Keep it up!", and return "You've got to push it just a bit harder!" otherwise. Call the function and print the return value.
-
-
+func pacing(currentDistance: Double, currentTime: Double, totalDistance: Double , goalTime: Double) -> String {
+    if calculatePace(currentDistance: currentDistance, currentTime: currentTime , totalDistance: totalDistance) <= goalTime {
+        var string = "Keep it up!"
+        print(string)
+        return "Keep it up!"
+    }else {
+        var string = "You've got to push it just a bit harder!"
+        print(string)
+        return "You've got to push it just a bit harder!"
+    }
+}
+pacing(currentDistance: 3000, currentTime: 50, totalDistance: 10000, goalTime: 120 )
 /*:
  _Copyright © 2023 Apple Inc._
 
